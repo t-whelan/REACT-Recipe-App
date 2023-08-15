4e6bbfd7424984867c2490036c5bad495ee1a96e
Arlincia <133870990+Arlincia@users.noreply.github.com>
2023-08-15 21:06:15 +0200

    Update Header.js

--- src/Header.js
+++ src/Header.js
@@ -5,6 +5,9 @@ const Header = () => {
     <header>
         <div className="title">
           <h1>Heritage Heaven</h1>
+           <hr/>
+            <br/>
+             <br/>
           <p>
             South Africa is a country of great cultural diversity. Such diversity is even reflected in South Africa's food recipes. South African recipes vary from simple traditional dishes to spicy curries and rich puddings. We are certain you will enjoy exploring the unique tastes of these recipes from South Africa.
           </p>
