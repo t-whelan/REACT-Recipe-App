5c629082a5b8ae6bce0e2b33d1f92294d711d698
Arlincia <133870990+Arlincia@users.noreply.github.com>
2023-08-15 19:55:16 +0200

    Update RecipeDetail.js

--- src/RecipeDetail.js
+++ src/RecipeDetail.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import './App.css';
+import './RecipeDetails.css';
 import { Link, useParams } from 'react-router-dom';
 
 const RecipeDetail = ({ recipes }) => {
@@ -11,25 +11,36 @@ const RecipeDetail = ({ recipes }) => {
   }
 
   return (
-    <div>
-      <h1>{recipe.title}</h1>
-      <img src={recipe.pictureUrl} alt={recipe.title} className="recipe-image" />
-
-      <p>{recipe.shortDescription}</p>
-      <h2>Ingredients:</h2>
+    <div className="container">
+      <div className="recipe-details">
+      <h1 className="title-recipe">{recipe.title}</h1>
+        <div className="row">
+            <div className="col-md-3">
+        <img src={recipe.pictureUrl} alt={recipe.title} className="recipe-image" />
+      <p className="description">{recipe.shortDescription}</p>
+  </div>
+  <div className="col-md-3">
+   <div className="ingredients-container">
+      <h2 className="ingredients">Ingredients:</h2>
       <ul>
         {recipe.ingredients.map((ingredient, index) => (
           <li key={index}>{ingredient}</li>
         ))}
-      </ul>
-      <h2>Preparation Steps:</h2>
-      <ol>
+       </ul>
+     </div>
+    </div>
+    </div>
+     <div className="steps-container">
+      <h2 className="steps">Preparation Steps:</h2>
+      <ol className="preparation-steps">
         {recipe.preparationSteps.map((step, index) => (
           <li key={index}>{step}</li>
         ))}
       </ol>
-      <Link to="/" className="button-link">Back to Recipes List</Link>
+        </div>
+      <Link to="/" className="back-to-recipes">Back to Recipes List</Link>
     </div>
+  </div>
   );
 };
 
