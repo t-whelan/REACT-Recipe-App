425152d9bda2aa21017bbedeabdb19328d3e2272
Arlincia <133870990+Arlincia@users.noreply.github.com>
2023-08-15 17:03:17 +0200

    Update RecipesList.js
    Added the recipe list to a card ,giving each element a className for appropriate styling

--- src/RecipesList.js
+++ src/RecipesList.js
@@ -3,12 +3,18 @@ import { Link } from 'react-router-dom';
 
 const RecipesList = ({ recipes }) => {
   return (
-    <div>
+    <div className="card-deck">
       <h1>Recipes List</h1>
       {recipes.map(recipe => (
-        <div key={recipe.id}>
-          <h2>{recipe.title}</h2>
-          <p>{recipe.shortDescription}</p>
+        <div key={recipe.id} className="card">
+        <div className="card-body">
+          <h2 className="card-title">{recipe.title}</h2>
+          <p className="card-text">{recipe.shortDescription}</p>
+              <img
+               src={recipe.pictureUrl}
+              className="card-img"
+             alt={recipe.title}/>
+               </div>
           <div className="centered-button-container">
   <Link to={`/recipe/${recipe.id}`} className="centered-button">View Recipe</Link>
 </div>
