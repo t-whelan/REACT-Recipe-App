cea5570ff6ff8d8454db417bd68fbe9ebf8299ec
Arlincia <133870990+Arlincia@users.noreply.github.com>
2023-08-15 21:26:00 +0200

    Update About.js

--- src/About.js
+++ src/About.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const About = () => {
   return (
-    <section className="about">
+    <div className="about">
       <h2>About Us</h2>
       <p>Welcome to our Recipe App. We provide delicious recipes for you to try at home.
           Heritage Heaven from South Africa is a web app made by Tionne,Thanyani and Arlincia in hopes that 
@@ -10,7 +10,7 @@ const About = () => {
             We want our South African heritage to be tasted and yes! we mean literally.
             We set up this recipe page for everyone who is keen on exploring what we prepare in our South African 
             kitchen in theirs.</p>
-    </section>
+    </div>
   );
 };
 
