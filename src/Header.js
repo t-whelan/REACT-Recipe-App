6e2fe5dfda2d4659eb6d82ac3c3450f60140c3c9
Tionne Whelan <80913553+t-whelan@users.noreply.github.com>
2023-08-16 10:54:47 +0200

    edits

--- src/Header.js
+++ src/Header.js
@@ -3,30 +3,30 @@ import React from "react";
 const Header = () => {
   return (
     <header>
-        <div className="title">
-          <h1>Heritage Heaven</h1>
-           <hr/>
-            <br/>
-             <br/>
-          <p>
-            South Africa is a country of great cultural diversity. Such diversity is even reflected in South Africa's food recipes. South African recipes vary from simple traditional dishes to spicy curries and rich puddings. We are certain you will enjoy exploring the unique tastes of these recipes from South Africa.
-          </p>
-        </div>
+      <div className="title">
+        <h1>Heritage Heaven</h1>
+        <hr/>
+        <br/>
+        <br/>
+        <p>
+          South Africa is a country of great cultural diversity. Such diversity is even reflected in South Africa's food recipes. South African recipes vary from simple traditional dishes to spicy curries and rich puddings. We are certain you will enjoy exploring the unique tastes of these recipes from South Africa.
+        </p>
       </div>
-     <div className="about">
-      <h2>About Us</h2>
-      <p>Welcome to our Recipe App. We provide delicious recipes for you to try at home.
-          Heritage Heaven from South Africa is a web app made by Tionne,Thanyani and Arlincia in hopes that 
-            our South African heritage can reach international heights and be celebrated around the world.
-            We want our South African heritage to be tasted and yes! we mean literally.
-            We set up this recipe page for everyone who is keen on exploring what we prepare in our South African 
-            kitchen in theirs.</p>
-    </div>
+      <div className="about">
+        <h2>About Us</h2>
+        <p>
+          Welcome to our Recipe App. We provide delicious recipes for you to try at home.
+          Heritage Heaven from South Africa is a web app made by Tionne, Thanyani, and Arlincia in hopes that our South African heritage can reach international heights and be celebrated around the world.
+          We want our South African heritage to be tasted and yes! we mean literally.
+          We set up this recipe page for everyone who is keen on exploring what we prepare in our South African kitchen in theirs.
+        </p>
+      </div>
     </header>
   );
 };
 
 export default Header;
+
 
 
 
--- src/RecipesList.js
+++ src/RecipesList.js
@@ -4,32 +4,36 @@ import Header from "./Header.js"
 
 const RecipesList = ({ recipes }) => {
   return (
-    <Header />
-    <div className="recipelist-title">
-    <h1>Recipes List</h1>
-    </div>
-    <br/>
-    <div className="card-deck">
-      {recipes.map(recipe => (
-        <div key={recipe.id} className="card">
-        <div className="card-body">
-          <h2 className="card-title">{recipe.title}</h2>
-          <p className="card-text">{recipe.shortDescription}</p>
+    <>
+      <Header />
+      <div className="recipelist-title">
+        <h1>Recipes List</h1>
+      </div>
+      <br/>
+      <div className="card-deck">
+        {recipes.map(recipe => (
+          <div key={recipe.id} className="card">
+            <div className="card-body">
+              <h2 className="card-title">{recipe.title}</h2>
+              <p className="card-text">{recipe.shortDescription}</p>
               <img
-               src={recipe.pictureUrl}
-              className="card-img"
-             alt={recipe.title}/>
-               </div>
-          <div className="centered-button-container">
-  <Link to={`/recipe/${recipe.id}`} className="centered-button">View Recipe</Link>
-</div>
-
-        </div>
-      ))}
-    </div>
+                src={recipe.pictureUrl}
+                className="card-img"
+                alt={recipe.title}
+              />
+            </div>
+            <div className="centered-button-container">
+              <Link to={`/recipe/${recipe.id}`} className="centered-button">View Recipe</Link>
+            </div>
+          </div>
+        ))}
+      </div>
+    </>
   );
 };
 
 export default RecipesList;
+
+
 
 
